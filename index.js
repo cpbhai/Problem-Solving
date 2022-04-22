bd53f1f3db9eb51e449effc82cff5cb2bbf855b1
Harshvardhan Singh <hvsreal.223@gmail.com>
2022-04-22 15:06:49 +0530

    added: linkedlist

--- index.js
+++ index.js
@@ -29,35 +29,105 @@ function readLine(type) {
   else line = line.split(/\s+/);
   return line;
 }
-function swap(a, x, y) {
-  const tmp = a[x];
-  a[x] = a[y];
-  a[y] = tmp;
-}
 
-function partition(a, l, h) {
-  const pivot = a[l];
-  let i = l,
-    j = h;
-  while (i < j) {
-    while (a[i] <= pivot) i++;
-    while (a[j] > pivot) j--;
-    if (i < j) swap(a, i, j);
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
   }
-  swap(a, j, l);
-  return j;
 }
 
-function quickSort(a, l, h) {
-  if (l < h) {
-    const pivot = partition(a, l, h);
-    quickSort(a, l, pivot - 1);
-    quickSort(a, pivot + 1, h);
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    const newNode = new Node(val);
+    ++this.length;
+    if (this.head == null) {
+      this.head = this.tail = newNode;
+      return this;
+    }
+    this.tail.next = newNode;
+    this.tail = newNode;
+    return this;
+  }
+  pop() {
+    if (this.head == null) return undefined;
+    let current = this.head,
+      newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    --this.length;
+    return current;
+  }
+  shift() {
+    if (this.head == null) return undefined;
+    const currHead = this.head;
+    this.head = currHead.next;
+    --this.length;
+    return currHead;
+  }
+  unshift(val) {
+    const newNode = new Node(val);
+    if (this.head == null) {
+      this.head = this.tail = newNode;
+      return this;
+    }
+    newNode.next = this.head;
+    this.head = newNode;
+    ++this.length;
+    return this;
+  }
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    let counter = 0;
+    let current = this.head;
+    while (counter != index) {
+      current = current.next;
+      ++counter;
+    }
+    return current;
+  }
+  set(index, val) {
+    const foundVal = this.get(index);
+    if (foundVal) {
+      foundVal.val = val;
+      return true;
+    }
+    return false;
+  }
+  insert(index, val) {
+    if (index < 0 || index > this.length) return false;
+    ++this.length;
+    if (index == this.length) {
+      this.push(val);
+      return true;
+    }
+    if (index == 0) {
+      this.unshift(val);
+      return true;
+    }
+    const newNode = new Node(val);
+    const prev = this.get(index - 1);
+    const temp = prev.next;
+    prev.next = newNode;
+    newNode.next = temp;
+    return true;
   }
 }
 
 function main() {
-  let a = [4, 6, 2, 5, 7, 9, 1, 3];
-  quickSort(a, 0, a.length - 1);
-  console.log(a);
+  const list = new SinglyLinkedList();
+  for (let x = 1; x <= 10; ++x) list.push(x);
+  // console.log(list.pop());
+  console.log(list);
+  list.insert(3, 100);
+  console.log(list);
 }
