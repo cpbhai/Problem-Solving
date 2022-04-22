b77a648b48001572b7b0d915c01278fa0c333e04
Harshvardhan Singh <hvsreal.223@gmail.com>
2022-04-22 13:04:25 +0530

    added: quick sort

--- index.js
+++ index.js
@@ -29,5 +29,35 @@ function readLine(type) {
   else line = line.split(/\s+/);
   return line;
 }
+function swap(a, x, y) {
+  const tmp = a[x];
+  a[x] = a[y];
+  a[y] = tmp;
+}
 
-function main() {}
+function partition(a, l, h) {
+  const pivot = a[l];
+  let i = l,
+    j = h;
+  while (i < j) {
+    while (a[i] <= pivot) i++;
+    while (a[j] > pivot) j--;
+    if (i < j) swap(a, i, j);
+  }
+  swap(a, j, l);
+  return j;
+}
+
+function quickSort(a, l, h) {
+  if (l < h) {
+    const pivot = partition(a, l, h);
+    quickSort(a, l, pivot - 1);
+    quickSort(a, pivot + 1, h);
+  }
+}
+
+function main() {
+  let a = [4, 6, 2, 5, 7, 9, 1, 3];
+  quickSort(a, 0, a.length - 1);
+  console.log(a);
+}
