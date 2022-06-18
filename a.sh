2686ea90027fd2bcf9f9cd1c725c69948ae3c703
Harsh <hvsreal.223@gmail.com>
2022-06-19 03:16:17 +0530

    As

--- a.sh
+++ a.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 clear
-COMPILE_BEGIN=$(date +%T)
+COMPILE_BEGIN=$(date +%M:%S.%3N)
 g++ main.cpp -o output -std=c++17
-EXECUTION_BEGIN=$(date +%T)
+EXECUTION_BEGIN=$(date +%M:%S.%3N)
 ./output
 echo $'\n'
-EXECUTION_END=$(date +%T)
-echo "${COMPILE_BEGIN} -> ${EXECUTION_BEGIN} -> ${EXECUTION_END}"
+EXECUTION_END=$(date +%M:%S.%3N)
+echo "${EXECUTION_END} > ${EXECUTION_BEGIN} > ${COMPILE_BEGIN}"
--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(0);
     vector<int> v;
-    for (int i = 1; i <= 1e1; i++)
+    for (int i = 1; i <= 1e5; i++)
     {
         v.push_back(i);
     }
