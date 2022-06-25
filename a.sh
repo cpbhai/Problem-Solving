b5cf6a296e45d2ff80db050fc2b29d245b8ebcbc
Harsh <hvsreal.223@gmail.com>
2022-06-25 19:04:50 +0530

    as

--- a.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-clear
-COMPILE_BEGIN=$(date +%M:%S.%3N)
-g++ main.cpp -o output -std=c++17
-EXECUTION_BEGIN=$(date +%M:%S.%3N)
-./output
-echo $'\n'
-EXECUTION_END=$(date +%M:%S.%3N)
-echo "${EXECUTION_END} > ${EXECUTION_BEGIN} > ${COMPILE_BEGIN}"
