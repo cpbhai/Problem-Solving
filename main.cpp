9537540414741070c5a0720c7cd57b200605981f
unknown <hvsreal.223@gmail.com>
2022-07-14 00:26:59 +0530

    added main

--- main.cpp
+++ main.cpp
@@ -2,9 +2,25 @@
 // g++ -std=c++17 main.cpp -o main.exe && timeout 2s ./main.exe<inp.txt>out.txt
 using namespace std;
 
+void solve()
+{
+    cout << "sd\n";
+}
+
+void submain()
+{
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        solve();
+    }
+}
+
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
+    submain();
     return 0;
 }
