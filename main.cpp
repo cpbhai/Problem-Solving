6332460809766e42539d9a5b2cfca6b8e5c31557
unknown <hvsreal.223@gmail.com>
2022-07-13 22:13:28 +0530

    added files

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+// g++ -std=c++17 main.cpp -o main.exe && timeout 2s ./main.exe<inp.txt>out.txt
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int x;
+    cin >> x;
+    while(true) cout << x << " m\n";
+    return 0;
+}
