657b027dbae32326f28d0055c00e0a7e2bdf6197
Harshvardhan Singh <hvsreal.223@gmail.com>
2022-08-26 01:26:31 +0530

    set up default temp

--- Main.java
+++ Main.java
@@ -1,19 +1,23 @@
 import java.util.Scanner;
 
 class Solution {
-    String solve() {
-        return "Harsh";
+    Scanner sc;
+
+    Integer solve() {
+        int x = sc.nextInt();
+        return x;
     }
+
     void harsh() {
+        sc = new Scanner(System.in);
         System.out.println(solve());
+        sc.close();
     }
 }
 
 public class Main {// javac Main.java && timeout 2s java Main<inp.txt>out.txt
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         Solution sol = new Solution();
         sol.harsh();
-        sc.close();
     }
 }
