955373b11216db6bf28c0c06ae14b2f0f83cd095
Harshvardhan Singh <hvsreal.223@gmail.com>
2022-08-26 01:17:05 +0530

    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

---
+++ Default.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Solution {
+    String solve() {
+        return "Harsh";
+    }
+    void harsh() {
+        System.out.println(solve());
+    }
+}
+
+public class Default {// javac Main.java && timeout 2s java Main<inp.txt>out.txt
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Solution sol = new Solution();
+        sol.harsh();
+        sc.close();
+    }
+}
