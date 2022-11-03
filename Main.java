bb5966bbe8a884bc4fa315d2a02a17e5445ec9d9
Harshvardhan Singh <hvsreal.223@gmail.com>
2022-11-03 11:59:05 +0530

    Basic Template

--- Main.java
+++ Main.java
@@ -1,34 +1,7 @@
 import java.util.Scanner;
 
-class Solution {
-    Scanner sc;
-    class Pair {
-        int x, y;
-
-        Pair(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        @Override
-        public String toString() {
-            return "[" + this.x + ", " + this.y + "]";
-        }
-    }
-
-    void solve() {
-    }
-
-    void harsh() {
-        sc = new Scanner(System.in);
-        solve();
-        sc.close();
-    }
-}
-
-public class Main {
+class Main {
     public static void main(String[] args) {
-        Solution sol = new Solution();
-        sol.harsh();
+        Scanner in = new Scanner(System.in);
     }
 }
